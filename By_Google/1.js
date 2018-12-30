6a8d0a3a5cdc66cdc33d25c310c85cd2804d8957
George Musk <42002018+gsmusk@users.noreply.github.com>
2018-12-30 09:34:49 -0800

    Update 1.js

--- By_Google/1.js
+++ By_Google/1.js
@@ -1,7 +1,7 @@
 /*
 This problem was asked by Google.
 Given a word W and a string S, find all starting indices in S which are anagrams of W.
-For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
+For example, given that W is "ab", and S is "abxaba", return 0, 3, and 5.
 */
 
 
